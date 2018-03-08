3b84de51a84466b6e1a1b1a086d420d95e9c4226
Azhypa <36773681+Azhypa@users.noreply.github.com>
2018-03-08 15:39:28 +0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,22 +1,23 @@
-import prompt from "@jace1995/readline";
-import {Primer} from "./logic";
+import prompt from '@jace1995/readline';
+import {Sample} from './logic';
 
  async function main() {
-    let score = 0;
-    while (true) {
-        const Current = new Primer();
-        const answ = await prompt (Current.text);
-        if (answ == "!exit") {
-            console.log(`Содержание алкоголя ~${score / 10}00 промилле!`);
-            break;
+        let score = 0;
+
+        while (true) {
+            const current = new Sample();
+            const answ = await prompt (current.text);
+            if (answ == "!exit") {
+                console.log(`Содержание алкоголя ~${score / 10}00 промилле!`);
+                break;
+            }
+            if (Number(answ) == current.result) {
+                console.log('Наливай еще!');
+            } else {
+                score++;
+                console.log(`Хватит пить!\nВерный ответ: ${current.result}`);
+            }
         }
-        if (Number(answ) == Current.result) {
-            console.log("Наливай еще!");
-        } else {
-            score++;
-            console.log(`Хватит пить!\nВерный ответ: ${Current.result}`);
-        }
-     }
 }
 main();
 
--- logic.ts
+++ logic.ts
@@ -7,44 +7,48 @@ enum Sign {
     MULTIPLY,
     DEV,
 }
+
 function operation(num1: number, num2: number, key: Sign): number {
-     switch (key) {
-         case Sign.ADD:
-            return num1 + num2; 
-         case Sign.SUBSTRACT:
-            return num1 - num2;           
-         case Sign.MULTIPLY:
-            return num1 * num2;     
-         case Sign.DEV: 
-            return Math.round(num1 / num2 * 10) / 10;  
-         default: 
-            return NaN;    
-     }
- }
+        switch (key) {
+            case Sign.ADD:
+                return num1 + num2; 
+            case Sign.SUBSTRACT:
+                return num1 - num2;           
+            case Sign.MULTIPLY:
+                return num1 * num2;     
+            case Sign.DEV: 
+                return Math.round(num1 / num2 * 10) / 10;     
+            }
+}
  
- function generate(max: number, min: number): number {
-         const res = random(min, max - 1);
-         return res ? res : max;
+function generate(max: number, min: number): number {
+        const res = random(min, max - 1);
+        return res ? res : max;
 }
+
 interface SignsPerformance {
-    [key : number]: string;
+        [key : number]: string;
 }
+
 const signsConvert: SignsPerformance = {
-    [Sign.ADD]: "+",
-    [Sign.SUBSTRACT]: "-",
-    [Sign.MULTIPLY]: "*",
-    [Sign.DEV]: "/",
+        [Sign.ADD]: '+',
+        [Sign.SUBSTRACT]: '-',
+        [Sign.MULTIPLY]: '*',
+        [Sign.DEV]: '/',
 };
-export class Primer {   
-    readonly text: string;
-    readonly result: number;
-    constructor() {
-        const min = -10;
-        const max = 10;
-        const key: Sign = <Sign> sample($enum(Sign).getValues());
-        const numberFirst = generate(min, max);
-        const numberSecond = generate(min, max);
-        this.result = operation(numberFirst, numberSecond, key);
-        this.text = `${numberFirst} ${signsConvert[key]} ${numberSecond} = `;
+
+export class Sample {   
+        readonly text: string;
+        readonly result: number;
+
+        constructor() {
+            const min = -10;
+            const max = 10;
+            const key: Sign = <Sign> sample($enum(Sign).getValues());
+            const numberFirst = generate(min, max);
+            const numberSecond = generate(min, max);
+
+            this.result = operation(numberFirst, numberSecond, key);
+            this.text = `${numberFirst} ${signsConvert[key]} ${numberSecond} = `;
     }
 }
