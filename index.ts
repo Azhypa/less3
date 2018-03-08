a6ce3032d3b3b45e749e15cce1d70c5c656548d9
Azhypa <36773681+Azhypa@users.noreply.github.com>
2018-03-08 21:54:51 +0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -2,24 +2,25 @@ import prompt from '@jace1995/readline';
 import {Sample} from './logic';
 
  async function main() {
-        let score = 0;
+  let score = 0;
 
-        while (true) {
-            const current = new Sample();
-            const answ = await prompt (current.text);
-            if (answ == "!exit") {
-                console.log(`Содержание алкоголя ~${score / 10}00 промилле!`);
-                break;
-            }
-            if (Number(answ) == current.result) {
-                console.log('Наливай еще!');
-            } else {
-                score++;
-                console.log(`Хватит пить!\nВерный ответ: ${current.result}`);
-            }
-        }
+  while (true) {
+    const current = new Sample();
+    const answ = await prompt (current.text);
+
+    if (answ == "!exit") {
+      console.log(`Содержание алкоголя ~${score / 10}00 промилле!`);
+      break;
+    }
+    if (Number(answ) == current.result) {
+      console.log('Наливай еще!');
+    } else {
+      score++;
+      console.log(`Хватит пить!\nВерный ответ: ${current.result}`);
+    }
+  }
 }
-main();
+ main();
 
 
 
